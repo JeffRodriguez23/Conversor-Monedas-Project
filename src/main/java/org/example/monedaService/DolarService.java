443830da21de3a659de319035b6997868779ea3d
JeffRodriguez23 <127356915+JeffRodriguez23@users.noreply.github.com>
2025-05-01 14:06:03 -0300

    Update DolarService.java

--- src/main/java/org/example/monedaService/DolarService.java
+++ src/main/java/org/example/monedaService/DolarService.java
@@ -6,9 +6,6 @@ import org.example.record.ChangeResult;
 
 public class DolarService extends ConversorMoneda {
 
-    private static final String apiKey = "f3a87325f9c83479e5f6734e";
-    private static final String urlBase = "https://v6.exchangerate-api.com/v6/";
-
     @Override
     protected String codigoBase() {
         return "USD";
